11fa38bbcd6ede01040bd0a36f41141b010ab09f
minako kondo <minakokondo@minakonoMacBook-Air.local>
2026-09-23 15:46:09 +0900

    Postモデルと関連付けを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_23_053340) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_23_062052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,19 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_23_053340) do
     t.datetime "created_at", null: false
     t.string "name", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.integer "emotion"
+    t.integer "favorite_rating"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +45,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_23_053340) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
 end
