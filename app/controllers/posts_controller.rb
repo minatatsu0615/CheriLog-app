669808ea699b88e2a542ded4445f7019c8f048ad
minako kondo <minakokondo@minakonoMacBook-Air.local>
2026-09-26 19:11:24 +0900

    投稿編集機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,20 @@ class PostsController < ApplicationController
     @post = current_user.posts.find(params[:id])
   end
 
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post, notice: "記録を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def new
     @post = Post.new
   end
